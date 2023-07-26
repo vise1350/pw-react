13b20bfbeae4f2db168aaa4ba3b617a3023daa3e
vise1350 <vise1350@gmail.com>
2023-07-26 13:26:51 +0530

    commit1

---
+++ src/Details.js
@@ -0,0 +1,8 @@
+import Person from "./Person";
+function Details(){
+    return(
+
+        <Person name="Ajay" age="23"/>
+    )
+}
+export default Details;
---
+++ src/Person.js
@@ -0,0 +1,11 @@
+
+function Person(props){
+    return(
+        <h1>The name of the person is {props.name} and the age is {props.age}</h1>
+
+
+
+    )
+}
+
+export default Person
